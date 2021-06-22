d0c3fbaedebf0dce3523c19942a6fcb0d0394b88
刘爽 <liushuang1@shihuituan.com>
2021-06-22 18:34:38 +0800

    架构

---
+++ src/views/hello/hello.js
@@ -0,0 +1,19 @@
+import Hello from './hello.vue'
+export default {
+  ...Hello,
+  data() {
+    return {
+      msg: '123213123---kkk'
+    }
+  },
+  created() {
+    console.log(123)
+  },
+  methods: {
+    handleTest() {
+      this.msg = this.$store.state.id
+    },
+    aa() {},
+    aaaa() {}
+  }
+}
---
+++ src/views/hello/hello.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hello">
+    <a-button @click="handleTest">{{ msg }}</a-button>
+  </div>
+</template>
+
+<style scoped>
+@import url('./hello.less');
+</style>
