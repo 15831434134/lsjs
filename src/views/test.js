97f2df1a5ab1b38360ca648ecba11f3fdd2bfe90
liushuang <liushuang1@shihuituan.com>
2021-07-31 14:43:11 +0800

    add 555

--- src/views/test.js
+++ src/views/test.js
@@ -1,10 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2021-07-31 14:27:19
- * @LastEditTime: 2021-07-31 14:38:58
+ * @LastEditTime: 2021-07-31 14:43:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \demo-01\src\views\test.js
  */
 console.log(123)
 console.log(789)
+console.log(555)
